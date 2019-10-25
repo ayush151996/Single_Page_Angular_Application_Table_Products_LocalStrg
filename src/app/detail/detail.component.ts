951ed70b03d213d2545d4dc8549d631db9738ef4
ayush151996 <ayured2011@gmail.com>
2019-10-25 11:56:48 +0530

    Injectable class for component Interaction
    Injectable class for component Interaction added.

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
@@ -7,11 +7,21 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
 })
 export class DetailComponent implements OnInit {
 
+  firstname;
+  description;
+  price;
+  
+  constructor(@Inject(MAT_DIALOG_DATA) public data) {
+    console.log(data);
+
+  }
 
 
   ngOnInit() {
+    this.firstname = this.data.fname;
+    this.price = this.data.prc;
   }
 
-  
+
 
 }
